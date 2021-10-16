be23f08ee8e6b602d90a46876f072dcda05eaba0
durgeshmeena <durgeshkumar@iitg.ac.in>
2021-10-16 21:18:39 +0530

    commiting repos

---
+++ NodeJs_heroku-demo/mlh-blog-live/app.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const path = require('path')
+const AutoLoad = require('fastify-autoload')
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  fastify.register(require('point-of-view'),{
+    engine:{
+      handlebars:require("handlebars")
+    },
+    layout: './templates/layout.hbs',
+    options: {
+      partials: {
+        header: './templates/partials/header.hbs',
+        footer: './templates/partials/footer.hbs'
+      }
+    }
+  })
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
